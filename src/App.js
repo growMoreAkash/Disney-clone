57f6af9f2020c414f68fe3582ee6d040684f2d77
growMoreAkash <akashgrowmore@gmail.com>
2023-10-25 17:55:57 +0530

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Login from './components/Login'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from './components/Header';
 function App() {
   return (
-    <d className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Akash2323
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </d>
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/Login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
